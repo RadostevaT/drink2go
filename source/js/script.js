abd83d7d9b9f9397072771f654ec1fa3c4fe1a1e
RadostevaT <radostevataisia@gmail.com>
2023-02-22 01:25:57 +0500

    starts header's style

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.site-list');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove ('site-list--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('site-list--closed')) {
+    navMain.classList.remove('site-list--closed');
+    navMain.classList.add('site-list--opened');
+  } else {
+    navMain.classList.add('site-list--closed');
+    navMain.classList.remove('site-list--opened');
+  }
+});
